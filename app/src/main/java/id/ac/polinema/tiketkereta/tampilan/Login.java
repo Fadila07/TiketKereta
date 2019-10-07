7ac9cb2896243b5fa3a5df2abdece9001df29dbe
Fadila07 <rfadila195@gmail.com>
2019-10-07 11:23:26 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polinema/tiketkereta/tampilan/Login.java
@@ -0,0 +1,16 @@
+package id.ac.polinema.tiketkereta.tampilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.polinema.tiketkereta.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/tiketkereta/tampilan/screen.java
@@ -0,0 +1,26 @@
+package id.ac.polinema.tiketkereta.tampilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import id.ac.polinema.tiketkereta.R;
+
+public class screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(screen.this, welcome.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
