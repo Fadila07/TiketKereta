78f2a632ea5726b7c9db3ca9cd757d8ac3fc196d
Fadila07 <rfadila195@gmail.com>
2019-10-08 22:41:18 +0700

    membuat form pemesanan

--- app/src/main/java/id/ac/polinema/tiketkereta/tampilan/Login.java
+++ app/src/main/java/id/ac/polinema/tiketkereta/tampilan/Login.java
@@ -2,15 +2,24 @@ package id.ac.polinema.tiketkereta.tampilan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import id.ac.polinema.tiketkereta.R;
+import id.ac.polinema.tiketkereta.fragment.Pemesanan;
 
 public class Login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
 }
